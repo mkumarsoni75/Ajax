d04142dedb36ecf2b6302aca408beae83c18df04
MITHUN KUMAR SONI <mkumarsoni75@gmail.com>
2020-12-25 16:26:31 +0530

    Consuming Api data

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 $(function(){
-$.ajax(){
-    
+    var $ordr = $('#orders');
+$.ajax({
+type: 'GET',
+url: 'https://jsonplaceholder.typicode.com/users',
+success: function(data){
+    console.log(data);
+    $.each(data,function(dataCount,data){
+        $ordr.append('<li> Name :'+data.name+'</li>');
+
+    });
 }
+
+})
 
 
 
