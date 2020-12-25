6c591f5657960ee1e79eaf50240df221f9874bb5
MITHUN KUMAR SONI <mkumarsoni75@gmail.com>
2020-12-25 16:54:16 +0530

    Api consumed

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(function(){
-    var $ordr = $('#orders');
+    var $ordr = $('#orders'); 
+    var $name = $('#name');
+    var $drink = $('#drink');
 $.ajax({
 type: 'GET',
 url: 'https://jsonplaceholder.typicode.com/users',
@@ -9,7 +11,34 @@ success: function(data){
         $ordr.append('<li> Name :'+data.name+'</li>');
 
     });
+},
+error: function(){
+    alert("There is an error");
 }
+});
+
+$('#add-order').on('click',function(){
+var order = {
+    name: $name.val(),
+    drink: $drink.val()
+};
+$.ajax({
+type:'POST',
+url: "https://jsonplaceholder.typicode.com/users",
+data: order,
+success: function(newData){
+    console.log(newData);
+    $ordr.append('<li> Name :'+newData.name+'</li>');
+  
+},
+error:function(data){
+    alert(data);
+}
+
+
+});
+
+
 
 })
 
